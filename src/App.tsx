c57fff7f72ec954b3c6541ac0e59c38b386d6b3a
Jaime Valdivia <dev@jaimevaldivia.com>
2024-11-23 19:44:47 -0600

    script to copy CNAME to dist on deploy

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <>
-      <h1>Welcome</h1>
+      <h1>Welcome!</h1>
       <p>My personal page is under construction.</p>
       <p className="info">@jhvaldivia</p>
     </>
